09152c1228f63a2f0ee6e2058a3018078755add2
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-20 15:53:17 -0300

    Função decodificadora adicionada
    Falta colocar uma estrutura repetitiva While no programa principal

--- Encryptor/Extensions/StringExtension.cs
+++ Encryptor/Extensions/StringExtension.cs
@@ -4,7 +4,7 @@ namespace System
 {
     public static class StringExtension
     {
-        private static string nomarlLetter = "qwertyuiopasdfghjklçzxcvbnmáéíóìàôõâãê";
+        private static string normalLetter = "qwertyuiopasdfghjklçzxcvbnmáéíóìàôõâãê";
         private static string codeLetter = "!9$%3@?/*8&56-:;0274^~[]{}|jsbghjvu1><";
         public static string Encrypt(this string s)
         {
@@ -12,9 +12,9 @@ namespace System
 
             for(int i = 0; i < s.Length; i++)
             {
-                for(int j = 0; j < nomarlLetter.Length; j++)
+                for(int j = 0; j < normalLetter.Length; j++)
                 {
-                    if(s[i] == nomarlLetter[j])
+                    if(s[i] == normalLetter[j])
                     {
                         text.Append(codeLetter[j]);
                         break;
@@ -22,6 +22,35 @@ namespace System
                     else if(Char.IsWhiteSpace(s[i]))
                     {
                         text.Append(".");
+                        break;
+                    }
+                    else if(Char.IsUpper(s[i]))
+                    {
+                        text.Append(s[i]);
+                        break;
+                    }
+                }
+            }
+
+            return text.ToString();
+        }
+
+        public static string DeCrypt(this string s)
+        {
+            StringBuilder text = new StringBuilder();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                for(int j = 0; j < codeLetter.Length; j++)
+                {
+                    if(s[i] == codeLetter[j])
+                    {
+                        text.Append(normalLetter[j]);
+                        break;
+                    }
+                    else if(s[i] == (".")[0])
+                    {
+                        text.Append(" ");
                         break;
                     }
                     else if(Char.IsUpper(s[i]))
--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -6,9 +6,29 @@ namespace Encryptor
     {
         static void Main(string[] args)
         {
-            Console.Write("Escreva o texto para codificar: ");
-            string word = Console.ReadLine();
-            Console.WriteLine(word.Encrypt());
+            Console.WriteLine("Selecione uma opção:");
+            Console.WriteLine("[1] Codificar");
+            Console.WriteLine("[2] Decodificar");
+            Console.WriteLine("[0] Sair");
+            Console.Write("Opção: ");
+            int n = int.Parse(Console.ReadLine());
+            string word;
+
+            switch (n)
+            {
+                case 1:
+                    Console.Write("Digite o texto para encriptar: ");
+                    word = Console.ReadLine();
+                    Console.WriteLine(word.Encrypt());
+                    break;
+                case 2:
+                    Console.Write("Digite o texto para desencriptar: ");
+                    word = Console.ReadLine();
+                    Console.WriteLine(word.DeCrypt());
+                    break;
+                case 0:
+                    break;
+            }
         }
     }
 }
