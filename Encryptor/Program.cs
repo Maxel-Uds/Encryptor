33482416733cd44831e45a5d807c52de0bfcf68b
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-21 10:35:57 -0300

    Tratamento de erros implementado, estrutura repetitiva e condicional (while, if) adicionadas
    Programa pronto

--- Encryptor/Program.cs
+++ Encryptor/Program.cs
@@ -6,29 +6,70 @@ namespace Encryptor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Selecione uma opção:");
-            Console.WriteLine("[1] Codificar");
-            Console.WriteLine("[2] Decodificar");
-            Console.WriteLine("[0] Sair");
-            Console.Write("Opção: ");
-            int n = int.Parse(Console.ReadLine());
             string word;
-
-            switch (n)
+            int n = 0;
+            bool error = false;
+            do
             {
-                case 1:
-                    Console.Write("Digite o texto para encriptar: ");
-                    word = Console.ReadLine();
-                    Console.WriteLine(word.Encrypt());
-                    break;
-                case 2:
-                    Console.Write("Digite o texto para desencriptar: ");
-                    word = Console.ReadLine();
-                    Console.WriteLine(word.DeCrypt());
-                    break;
-                case 0:
-                    break;
-            }
+                try
+                {
+                    do
+                    {
+                        error = false;
+                        Console.WriteLine("Selecione uma opção:");
+                        Console.WriteLine("[1] Codificar");
+                        Console.WriteLine("[2] Decodificar");
+                        Console.WriteLine("[0] Sair");
+                        Console.Write("Opção: ");
+                        n = int.Parse(Console.ReadLine());
+
+                        if(n != 1 && n != 2 && n != 0)
+                        {
+                            Console.WriteLine($"A opção {n} é inválida!");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                    } while(n != 1 && n != 2 && n != 0);
+
+                    switch (n)
+                    {
+                        case 1:
+                            Console.Write("Digite o texto para encriptar: ");
+                            word = Console.ReadLine();
+                            Console.WriteLine(word.Encrypt());
+                            break;
+                        case 2:
+                            Console.Write("Digite o texto para desencriptar: ");
+                            word = Console.ReadLine();
+                            Console.WriteLine(word.DeCrypt());
+                            break;
+                        case 0:
+                            break;
+                    }
+
+                    if(n != 0)
+                    {
+                        do {
+                            Console.Write("Para codificar outro texto digite 3, para sair digite 0: ");
+                            n = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            if(n != 3 && n != 0)
+                            {
+                                Console.WriteLine($"A opção {n} é inválida!");
+                                Console.ReadLine();
+                                Console.Clear();
+                            }
+                        } while(n != 3 && n != 0);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                    Console.ReadLine();
+                    Console.Clear();
+                    error = true;
+                }
+            } while((n != 3 && error) || n == 3);
         }
     }
 }
